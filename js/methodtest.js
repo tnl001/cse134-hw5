232759467170fbbd0423dff1d38a5f534fd3b8d6
Tai Le <tnl001@ucsd.edu>
2023-03-12 23:25:10 -0700

    added GET, PUT, and DELETE

--- js/methodtest.js
+++ js/methodtest.js
@@ -8,21 +8,54 @@ function init() {
 
     let output = document.getElementById("response");
 
+    let articleDate = document.getElementById("date");
+    let date = new Date().toISOString();
+    articleDate.value = date.substring(0, 10);
+
+
     postBtn.addEventListener("click", async (e) => {
         e.preventDefault();
         let data = collectData();
         let res = await methodPOST(data);
         displayData(output, res);
     });
+
+    getBtn.addEventListener("click", async (e) => {
+        e.preventDefault();
+        let data = collectData();
+        console.log(data)
+        let res = await methodGET(data);
+        displayData(output, res);
+    });
+
+    putBtn.addEventListener("click", async (e) => {
+        e.preventDefault();
+        let data = collectData();
+        let res = await methodPUT(data);
+        displayData(output, res)
+    });
+
+    deleteBtn.addEventListener("click", async (e) => {
+        e.preventDefault();
+        let data = collectData();
+        let res = await methodDELETE(data);
+        displayData(output, res)
+    });
 }
 
+/**
+ * POST Request
+ * @param {*} data 
+ * @returns Data promise
+ */
 async function methodPOST(data) {
-    let res = await fetch("https://httpbin.org/post", {
+    let requestString = "https://httpbin.org/post";
+    let res = await fetch(requestString, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: data
+        body: JSON.stringify(data)
     });
 
     if (!res.ok) {
@@ -35,6 +68,71 @@ async function methodPOST(data) {
 }
 
 /**
+ * GET Request
+ * @param {*} data 
+ * @returns Data promise
+ */
+async function methodGET(data) {
+    let requestString = `https://httpbin.org/get?id=${data.id}&article_name=${data.article_name}&article_body=${data.article_body}&date=${data.date}`;
+    let res = await fetch(requestString, {
+        method: "GET"
+    });
+
+    if (!res.ok) {
+        console.log(res.status);
+    }
+
+    let jsonPromise = await res.json();
+
+    return jsonPromise;
+}
+
+/**
+ * PUT Request
+ * @param {*} data 
+ * @returns Data promise
+ */
+async function methodPUT(data) {
+    let requestString = "https://httpbin.org/put";
+    let res = await fetch(requestString, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (!res.ok) {
+        console.log(res.status);
+    }
+
+    let jsonPromise = await res.json();
+
+    return jsonPromise;
+}
+
+/**
+ * DELETE Request
+ * @param {*} data 
+ * @returns Data promise
+ */
+async function methodDELETE(data) {
+    let requestString = `https://httpbin.org/delete?id=${data.id}&article_name=${data.article_name}&article_body=${data.article_body}&date=${data.date}`;
+    let res = await fetch(requestString, {
+        method: "DELETE"
+    });
+
+    if (!res.ok) {
+        console.log(res.status);
+    }
+
+    let jsonPromise = await res.json();
+
+    return jsonPromise;
+}
+
+
+/**
  * Helper function to collect data from form
  * @returns JSON data object
  */
@@ -42,14 +140,15 @@ function collectData() {
     let articleID = document.getElementById("id");
     let articleName = document.getElementById("article_name");
     let articleBody = document.getElementById("article_body");
-    let articleDate = new Date().toISOString();
 
-    let data = JSON.stringify({
+
+
+    let data = {
         "id": articleID.value,
         "article_name": articleName.value,
         "article_body": articleBody.value,
         "date": articleDate
-    });
+    };
 
     return data;
 }
